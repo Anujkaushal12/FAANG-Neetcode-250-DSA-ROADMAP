db655e9fcc0802ea0ff6fc1d964a417bb67041b8
Anuj Kaushal <lpsanuj5262@gmail.com>
2026-05-05 16:44:03 +0530

    Two sum problem soln using hashmap with tiem complexity O(n)

---
+++ 01_Array_hashing_pattern/03_Two_Sum.py
@@ -0,0 +1,34 @@
+"""
+Problem: LeetCode 1 - Two Sum
+
+Key Idea:
+The key idea to solve this problem efficiently is by using a hash map (dictionary in Python) to keep track of the elements we have traversed so far. 
+For each element in the input list, we calculate the difference between the target and the current element. 
+If this difference exists in the hash map, then we have found the pair that sums to the target, and we return their indices. 
+Otherwise, we add the current element to the hash map and continue with the next element.
+
+Detailed approach:
+1.Iterate: You go through the list once, keeping track of the current number (n) and its index (i).
+2.Calculate the Difference: For each number, you calculate diff = target - n. This is the specific value needed to reach the goal.
+3.Check the Map:
+  (i)If diff is already in your hashset, you've found the pair! You return the stored index of diff and your current index i.
+  (ii)If it’s not there, you add the current number and its index to the map (hashset[n] = i) and move to the next step
+"""
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashset={}  # val -> index
+        for i, n in enumerate(nums):
+            diff= target- n
+            if diff in hashset:
+                return hashset[diff],i
+            hashset[n]=i
+        
+"""
+Time Complexity:
+The time complexity of this solution is O(n), where n is the size of the input array 'nums'. In the worst case, we might need to traverse the entire array once to find the pair.
+
+Space Complexity:
+The space complexity is O(n), as the hash map can potentially store all elements of the input array 'nums' if they are all unique. In the best case, where the target sum is achieved with the first two elements, the space complexity would be O(1). However, in the worst case, if all elements are distinct, the hash map will store all elements, leading to O(n) space complexity.
+"""
